cd9b333bb0d73bfa113d197619ec898183c33537
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-04-12 17:54:27 +0200

    Create simple Genre model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412154923) do
+ActiveRecord::Schema.define(version: 20160412155314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20160412154923) do
   add_index "cinemas", ["city_id"], name: "index_cinemas_on_city_id", using: :btree
 
   create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
