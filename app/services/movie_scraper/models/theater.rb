c152d349652f999388108d549c319c2e0640fae9
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-04-24 19:34:50 +0200

    Add find_or_create_theaters method

--- app/services/movie_scraper/models/theater.rb
+++ app/services/movie_scraper/models/theater.rb
@@ -1,17 +1,26 @@
 module MovieScraper
   module Models
     class Theater
-      attr_accessor :where, :movies
+      attr_accessor :name, :movies, :city_name
 
-      def initialize(where, movies)
-        @where = where
+      def initialize(name, movies, city_name)
+        @name = name
         @movies = movies
+        @city_name = city_name
+        find_or_create_theaters
       end
 
       def to_h
         hash = {}
         instance_variables.each {|var| hash[var.to_s.delete("@")] = instance_variable_get(var) }
         hash
+      end
+
+      private
+
+      def find_or_create_theaters
+        city = ::City.find_by(name: city_name)
+        ::Theater.find_or_create_by(name: name, city: city) unless name.empty?
       end
     end
   end
