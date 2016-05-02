90b0bc1456b9d5589ad01f7fd3bde2357442cd3f
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-02 13:35:26 +0200

    Link to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :admins
 
   resources :admins, only: :index
+  resources :users do
+    get '/profile', to: 'users#show'
+  end
 
   root 'home#index'
 end
