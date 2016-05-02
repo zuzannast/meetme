59412f73637774f3c6290df9c9562bf5ede4b43b
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-02 18:38:24 +0200

    Permit showtime in creating event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,6 +15,9 @@ class EventsController < ApplicationController
   end
 
   def create
+    binding.pry
+    @event = Event.new(event_params)
+
     if @event.save
       redirect_to event_path(@event)
     else
@@ -36,5 +39,11 @@ class EventsController < ApplicationController
   def destroy
     event.destroy
     redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:description, :date, :showtime_id)
   end
 end
