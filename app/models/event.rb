e4e169207bddecdef7011c63969cda5c1b78d44a
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-02 13:23:53 +0200

    Event has one showtime

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
   has_many :users, through: :events_users
+  has_one :showtime
 end
--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -1,4 +1,5 @@
 class Showtime < ActiveRecord::Base
   belongs_to :theater
   belongs_to :movie
+  belongs_to :event
 end
