87848531547c68ab6f9655f1c3ad50bc68ff6256
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-02 15:33:56 +0200

    Add user edit profile method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  def edit
+  end
+
+  def update
+    user = User.find(current_user.id)
+
+    if user.save
+      redirect_to user_profile_path(user)
+    else
+      render :edit
+    end
+  end
 end
