9ed518046124f708b8bbdab516f67bb3ece31bcc
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-07 13:54:06 +0200

    Create Greet component and use it in Main

---
+++ app/react/components/greet.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Greet extends React.Component {
+  render() {
+    return <h2>Hello World</h2>;
+  }
+}
--- app/react/components/main.jsx
+++ app/react/components/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Greet from './greet';
 
 class Main extends React.Component {
   render() {
     return (
+      <Greet />
     );
   }
 }
