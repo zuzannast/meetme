7f4c1ca63b99048b87744e35890f67f8435a8d4a
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-14 16:21:00 +0200

    Move mdl-layout class to the yield

--- app/react/components/main.jsx
+++ app/react/components/main.jsx
@@ -4,7 +4,7 @@ import EventBox from './event_box';
 class Main extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className=".mdl-layout__content">
       <h1>Hello from the React</h1>
         <EventBox />
       </div>
