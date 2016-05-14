6fe4c7339fe31e00737e56ca158efd7365109723
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-14 16:17:38 +0200

    Style the navigation a little bit

--- app/react/components/event_box.jsx
+++ app/react/components/event_box.jsx
@@ -7,7 +7,7 @@ export default class EventBox extends React.Component {
         <form>
           <div className="mdl-textfield mdl-js-textfield">
             <textarea className="mdl-textfield__input" />
-            <label className="mdl-textfield__label">What goin' on?</label>
+            <label className="mdl-textfield__label">What's going on?</label>
           </div>
           <button className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored">
             <i className="material-icons">add</i>
