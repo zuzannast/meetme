8eba19b006d935eba80264e20a79a77f172ba0d1
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-20 16:02:07 +0200

    Style user edit form

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   resources :admins, only: :index
   resources :users do
     get '/profile', to: 'users#show'
+    post '/profile', to: 'user#update'
   end
-  
+
   resources :events
 
   root 'home#index'
