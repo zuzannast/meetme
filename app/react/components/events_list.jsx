ce15f40c60829201395e22599a34b2105d527deb
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-20 11:24:41 +0200

    Render only Event objects

--- app/react/components/events_list.jsx
+++ app/react/components/events_list.jsx
@@ -7,32 +7,8 @@ export default class EventBox extends React.Component {
       <div>
         <ul className="demo-list-three mdl-list">
           <Event />
-          <li className="mdl-list__item mdl-list__item--three-line">
-            <span className="mdl-list__item-primary-content">
-              <i className="material-icons  mdl-list__item-avatar">person</i>
-              <span>Aaron Paul</span>
-              <span className="mdl-list__item-text-body">
-                Aaron Paul played the role of Jesse in Breaking Bad. He also featured in
-                the "Need For Speed" Movie.
-              </span>
-            </span>
-            <span className="mdl-list__item-secondary-content">
-              <a className="mdl-list__item-secondary-action" href="#"><i className="material-icons">star</i></a>
-            </span>
-          </li>
-          <li className="mdl-list__item mdl-list__item--three-line">
-            <span className="mdl-list__item-primary-content">
-              <i className="material-icons  mdl-list__item-avatar">person</i>
-              <span>Bob Odenkirk</span>
-              <span className="mdl-list__item-text-body">
-                Bob Odinkrik played the role of Saul in Breaking Bad. Due to public fondness for the
-                character, Bob stars in his own show now, called "Better Call Saul".
-              </span>
-            </span>
-            <span className="mdl-list__item-secondary-content">
-              <a className="mdl-list__item-secondary-action" href="#"><i className="material-icons">star</i></a>
-            </span>
-          </li>
+          <Event />
+          <Event />
         </ul>
       </div>
     );
