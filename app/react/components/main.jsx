53d998168537121b84605d50ea4fabc965bcbdaf
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-22 11:10:49 +0200

    Remove the React headline

--- app/react/components/main.jsx
+++ app/react/components/main.jsx
@@ -17,7 +17,6 @@ class Main extends React.Component {
   render() {
     return (
       <div className=".mdl-layout__content">
-      <h1>Hello from the React</h1>
         <EventBox sendEvent={ this.addEvent.bind(this) } />
         <EventsList events={ this.state.eventsList } />
       </div>
