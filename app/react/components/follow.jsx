923505ee1c80adb1d82888e0071a24843fbcb069
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-26 13:49:33 +0200

    Create simple Follow component

---
+++ app/react/components/follow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Follow extends React.Component {
+  render() {
+    return (
+      <div className="demo-list-three">
+        <h3>Who to follow</h3>
+      </div>
+    );
+  }
+}
