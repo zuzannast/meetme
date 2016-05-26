fabbdb885835dfda8cd592dea5a41003560a4c72
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-26 15:34:47 +0200

    Move library imports higher

--- app/react/components/main.jsx
+++ app/react/components/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { render } from 'react-dom'
+import { Router, Route, Link, browserHistory } from 'react-router';
+
 import App from './app';
 import Index from './index';
 import Follow from './follow';
-
-import { render } from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router';
 
 export default class Main extends React.Component {
   render() {
