b3c7d4157e5b4759896ed80233dbf9d0dbe61896
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-26 17:39:41 +0200

    Add who_to_follow scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
  def as_json(options={})
    { id: id, name: self.decorate.display_name, gravatar: self.decorate.gravatar }
  end
+
+ def self.who_to_follow(current_user_id)
+   where(["id != :current_user_id and not exists (
+       select 1 from followers
+       where user_id = users.id
+       and followed_by = :current_user_id
+     )", { current_user_id: current_user_id }])
+    .order("random()").all
+ end
 end
