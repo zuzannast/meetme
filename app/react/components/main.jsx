894050edc5873cbc22935250eaa528f2352c2c56
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-26 16:13:50 +0200

    Create Main Router

--- app/react/components/main.jsx
+++ app/react/components/main.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom'
 import { Router, Route, Link, browserHistory } from 'react-router';
 
 import App from './app';
 import Index from './index';
 import Follow from './follow';
 
-export default class Main extends React.Component {
-  render() {
-    return(
-      <Router history={browserHistory}>
-        <Route component={App}>
-          <Route path="/" component={Index} />
-          <Route path="/follow" component={Follow} />
-        </Route>
-      </Router>
-    )
-  }
-}
+let Main =
+  <Router history={browserHistory}>
+    <Route component={App}>
+      <Route path="/" component={Index} />
+      <Route path="/follow" component={Follow} />
+    </Route>
+  </Router>
+
+export default Main;
