fb9db82266a6a143634c20db3933da16ace9c0a1
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-26 21:09:22 +0200

    Add title to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526090907) do
+ActiveRecord::Schema.define(version: 20160526190805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160526090907) do
     t.datetime "updated_at",   null: false
     t.integer  "showtime_id"
     t.integer  "organiser_id"
+    t.string   "title"
   end
 
   create_table "events_users", force: :cascade do |t|
