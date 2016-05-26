deb82ca1d01a7d886a39300f25a32b1cad4d30cf
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-05-26 11:34:23 +0200

    User react-router to handle paths

--- app/react/main.jsx
+++ app/react/main.jsx
@@ -1,9 +1,30 @@
+import React form 'react';
+import ReactDOM from 'react-dom';
 import Index from '.components/index';
+
+import { Router, Route, link } from 'react-router';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
 
 let documentReady = () => {
   let reactNode = document.getElementById('react');
   if (reactNode) {
-    React.render(<Index />, reactNode);
+    ReactDOM.render(
+      <Router>
+        <Route component={App}>
+          <Route path="/" component={Index} />
+        </Route>
+      </Router>
+    , reactNode);
   }
 };
 
