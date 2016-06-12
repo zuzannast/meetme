1a4f619c729d25d7179aa80041b8f63e7399a0c8
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-06-12 12:29:45 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+### Admin and Users
+
+Admin.create!(
+  email: 'admin@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  first_name: 'Admin',
+  last_name: 'Example'
+)
+
+User.create!(
+  email: 'samer.buna@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  first_name: 'Samer',
+  last_name: 'Buma'
+)
+
+User.create!(
+  email: 'aaron.patterson@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  first_name: 'Aaron',
+  last_name: 'Patterson'
+)
 
 cities = City.create([{ name: 'Kraków' }, { name: 'Warszawa' }])
 
 theaters = Theater.create([
   { name: 'ARS Krakowskie Centrum Kinowe-Aneks', city: City.first },
-  { name: 'Kino Agrafka', city: City.first }
+  { name: 'Kino Agrafka', city: City.first },
+  { name: 'Kino Atlantic', city: City.last },
+  { name: 'Paradiso', city: City.last }
 ])
