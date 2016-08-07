7a70b75325eb0e5ecb935343165c1576535d513e
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-08-07 21:08:02 +0200

    Send info about participantIds

--- app/react/stores/event_store.jsx
+++ app/react/stores/event_store.jsx
@@ -3,7 +3,7 @@ import ActionTypes from '../constants';
 import AppEventEmitter from './app_event_emitter';
 
 let _events = [];
-let _participantIds = [];
+let _joinedIds = [];
 
 class EventEventEmitter extends AppEventEmitter {
   getAll() {
@@ -16,7 +16,8 @@ class EventEventEmitter extends AppEventEmitter {
   getOne(eventId) {
     return _events.map(event => {
       if (event.id === parseInt(eventId))
-        event.joined = _participantIds.indexOf(event.id) >= 0;
+        _joinedIds = event.participants.map(p => { return p.event_id } );
+        event.joined = _joinedIds.indexOf(event.id) >= 0;
         return event;
       }
     ).filter( function(n){ return n != undefined })[0]
@@ -36,7 +37,7 @@ AppDispatcher.register( action => {
       EventStore.emitChange();
       break;
     case ActionTypes.RECEIVED_ONE_PARTICIPANT:
-      _participantIds.push(action.rawParticipant.event_id);
+      _joinedIds.push(action.rawParticipant.event_id);
       EventStore.emitChange();
       break;
     default:
