d23318f99df8783441bc3f38a17d128bc9d066ca
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-08-07 19:50:09 +0200

    Remove warning of elements not found

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,15 +1,21 @@
 $( document ).ready(function() {
+  var datetimeInputField = document.getElementById('datetime-input-field');
+  var datetimeSelect = document.getElementById('datetime-select');
   var dialog = new mdDateTimePicker['default']({
     type: 'date',
-    trigger: document.getElementById('datetime-input-field')
+    trigger: datetimeInputField
    });
 
-  document.getElementById('datetime-select').addEventListener('click', function() {
-    dialog.toggle();
-  });
+  if (datetimeSelect){
+    document.getElementById('datetime-select').addEventListener('click', function() {
+      dialog.toggle();
+    });
+  };
 
-  document.getElementById('datetime-input-field').addEventListener('onOk', function() {
-    this.value = dialog.time.format("dddd, MMMM Do YYYY");
-    $('#date-field').removeClass('hidden');
-  });
+  if (datetimeInputField) {
+    datetimeInputField.addEventListener('onOk', function() {
+      this.value = dialog.time.format("dddd, MMMM Do YYYY");
+      $('#date-field').removeClass('hidden');
+    });
+  };
 });
