6d31a21c15517c0ce9d61f49b829a59ee30ec8ff
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-08-14 20:06:56 +0200

    Update routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  resources :followers do
-    collection do
-      get 'random'
-    end
-  end
-
-  resources :participants
-
   devise_for :users
   devise_for :admins
+
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
+      resources :events, only: [:show, :index]
+      resources :followers
+      resources :comments
+      resources :participants
+
+      root 'home#index'
+    end
+  end
 
   resources :admins, only: :index
   resources :users do
@@ -16,8 +19,16 @@ Rails.application.routes.draw do
     post '/profile', to: 'user#update'
   end
 
-  resources :events
+  resources :events, except: [:show, :index]
+
+  resources :followers do
+    collection do
+      get 'random'
+    end
+  end
+
   resources :comments
+  resources :participants
 
   root 'home#index'
 end
