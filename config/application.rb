ca1d1ab44ed278e5c20c6c7057f67eac1a989a7a
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-08-15 17:44:51 +0200

    Add some configs

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +11,7 @@ Bundler.require(*Rails.groups)
 module Meetme
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+    config.api_only = false
 
     config.generators do |g|
       g.factory_girl false
