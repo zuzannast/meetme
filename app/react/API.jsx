7fe601a09315ec3e205b0c2c394ebb4ebfd5d23d
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-08-15 19:06:53 +0200

    Routes fixes

--- app/react/API.jsx
+++ app/react/API.jsx
@@ -2,49 +2,49 @@ import ServerActions from "./actions/server_actions"
 
 export default {
   getAllEvents() {
-    $.get("/events")
+    $.get("/app/events")
     .success( rawEvents => ServerActions.receivedEvents(rawEvents))
     .error(error => console.log(error));
   },
   getOneEvent(eventId) {
-    $.get("/events", { event_id: eventId })
+    $.get("/app/events", { event_id: eventId })
     .success( rawEvent => ServerActions.receivedOneEvent(rawEvent))
     .error(error => console.log(error));
   },
   createEvent(description) {
-    $.post("/events", { description })
+    $.post("/app/events", { description })
     .success( rawEvent => ServerActions.receivedOneEvent(rawEvent))
     .error(error => console.log(error));
   },
   joinEvent(eventId) {
-    $.post("/participants", { event_id: eventId })
+    $.post("/app/participants", { event_id: eventId })
     .success( rawParticipant => ServerActions.receivedOneParticipant(rawParticipant))
     .error(error => console.log(error));
   },
   leaveEvent(eventId) {
     $.ajax({
-      url: "/participants/" + eventId,
+      url: "/app/participants/" + eventId,
       method: 'DELETE'
     }).done(rawParticipant => ServerActions.removedOneParticipant(rawParticipant))
     .fail(error => console.log(error));
   },
   getAllUsers() {
-    $.get("/followers/random")
+    $.get("/app/followers/random")
     .success( rawUsers => ServerActions.receivedUsers(rawUsers))
     .error(error => console.log(error));
   },
   followUser(userId) {
-    $.post("/followers", { user_id: userId })
+    $.post("/app/followers", { user_id: userId })
     .success( rawFollower => ServerActions.receivedOneFollower(rawFollower))
     .error(error => console.log(error));
   },
   getAllComments(eventId) {
-    $.get("/comments", { event_id: eventId })
+    $.get("/app/comments", { event_id: eventId })
     .success( rawComments => ServerActions.receivedComments(rawComments))
     .error(error => console.log(error));
   },
   createComment(body, eventId) {
-    $.post("/comments", { body, eventId })
+    $.post("/app/comments", { body, eventId })
     .success( rawComment => ServerActions.receivedOneComment(rawComment))
     .error(error => console.log(error));
   },
