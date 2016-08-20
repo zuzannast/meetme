1f6144f106f5b6318d1014b967acc6ee756a8ce2
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-08-20 14:28:44 +0200

    Improve UserDecorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,11 +2,17 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def display_name
-    object.first_name.present? ? "#{first_name} #{last_name.chars.first}." : object.email.split('@').first
+    name_present? ? "#{first_name} #{last_name.chars.first}." : object.email.split('@').first
   end
 
   def gravatar
     hash = Digest::MD5.hexdigest(object.email)
-    "http://www.gravatar.com/avatar/#{hash}"
+    "http://www.gravatar.com/avatar/#{hash}?size=256"
+  end
+
+  private
+
+  def name_present?
+    object.first_name.present? && object.last_name.present?
   end
 end
