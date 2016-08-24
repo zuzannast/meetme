65d5173fa6505f3b321fc4af1850c06ba0191b86
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-08-24 19:23:18 +0200

    Update profile traits on user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,25 +7,38 @@ class UsersController < ApplicationController
   expose :users, -> { User.all }
   expose :cities, -> { cities_for_select }
   expose :genres, -> { get_genres }
+  expose :traits, -> { get_traits }
 
   def edit
     user.build_profile unless profile
   end
 
   def update
-    user.update(user_params)
-    redirect_to user_profile_path(user, profile)
+    if user.update(user_params)
+      create_profile_traits
+      redirect_to user_profile_path(user, profile)
+    else
+      redirect_to root_path
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(profile_attributes: [:first_name, :last_name, :city_id, genre_ids: []])
+    params.require(:user).permit(
+      profile_attributes: [:id, :first_name, :last_name, :city_id, { genre_ids: [] }, trait_attributes: { id: {}, value: {} } }]
+    )
   end
 
   def get_genres
-    Genre.all.collect do |g|
-      [ g.name, g.id ]
-    end
+    Genre.pluck(:name, :id)
+  end
+
+  def get_traits
+    Trait.all
+  end
+
+  def create_profile_traits
+    Users::CreateProfileTraits.new(profile, params).call
   end
 end
