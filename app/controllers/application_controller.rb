9693e8c079d3a40b97dc42799081b3e830a9b6f7
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-09-18 17:08:15 +0200

    Ask user to update city

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # expose :user, -> { UserDecorator.new(current_user) }
+  include ApplicationHelper
 
   def redirect_to_app
     if user_signed_in?
-      redirect_to app_path
+      redirect_to_app_or_profile
     else
       redirect_to events_list_path
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
   def events_list_path
     '/events_list'
   end
+
+  def redirect_to_app_or_profile
+    if profile_not_updated?
+      redirect_to user_profile_path(current_user.id), flash: { notice: update_city_message }
+    else
+      redirect_to app_path
+    end
+  end
+
+  private
+
+  def profile_not_updated?
+    current_user.profile.city.nil?
+  end
+
+  def update_city_message
+    "Seems like you haven't set your city yet. Please do it before continuing."
+  end
 end
